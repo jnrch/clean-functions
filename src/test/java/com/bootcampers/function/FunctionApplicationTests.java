821565234f8e00454e52e40a79b265f69c157d60
jnrch41 <jonathanrojas31@gmail.com>
2022-07-03 22:41:05 -0300

    first commit

---
+++ src/test/java/com/bootcampers/function/FunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcampers.function;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
